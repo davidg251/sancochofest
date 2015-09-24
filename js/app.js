ef247a6a7c98d5cb97095ccdd236a3c889e43808
David Gomez <david.ernesto.gomez@correounivalle.edu.co>
2015-09-24 17:47:18 -0500

    cambios menores en cuanto a rendimiento

--- js/app.js
+++ js/app.js
@@ -1,40 +1,45 @@
-$( document ).ready(function(){
+$(window).load(function() {
+
+    $(".loader").fadeOut("slow");
+    $( document ).ready(function(){
 
 
-  $('.button-collapse').sideNav({
-      menuWidth: 500, // Default is 240
+      $('.button-collapse').sideNav({
+          menuWidth: 500, // Default is 240
 
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-   $('.parallax').parallax();
+          closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+      );
+       $('.parallax').parallax();
 
-   window.addEventListener('scroll', function(e){
-     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-         shrinkOn = 400,
-         header = $('#logo')
-     if (distanceY > shrinkOn) {
-         $('#logo').addClass('pequenio');
-     } else {
-         if ( $('#logo').hasClass('pequenio')) {
-              $('#logo').removeClass('pequenio')
+       window.addEventListener('scroll', function(e){
+         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+             shrinkOn = 400,
+             header = $('#logo')
+         if (distanceY > shrinkOn) {
+             $('#logo').addClass('pequenio');
+         } else {
+             if ( $('#logo').hasClass('pequenio')) {
+                  $('#logo').removeClass('pequenio')
+             }
          }
-     }
- });
+     });
 
- $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
-      var target = this.hash;
-      var $target = $(target);
+     $('a[href^="#"]').on('click',function (e) {
+          e.preventDefault();
+          var target = this.hash;
+          var $target = $(target);
 
 
-      $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
+          $('html, body').stop().animate({
+              'scrollTop': $target.offset().top
 
-      }, 900, 'swing', function () {
+          }, 900, 'swing', function () {
 
-          window.location.hash = target;
+              window.location.hash = target;
+          });
       });
-  });
 
-})
+    })
+
+});
