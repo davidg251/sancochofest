67505e971794c2d1c16dea740cb45d304df52064
David Gomez <david.ernesto.gomez@correounivalle.edu.co>
2015-10-05 18:48:43 -0500

    muchos cambios se aniade temporal html para hacer deploy rapido, validacion no OK, no script php ,grandes cambios, falta mucho aun

--- js/app.js
+++ js/app.js
@@ -23,19 +23,26 @@ $(window).load(function() {
              header = $('#logo')
          if (distanceY > shrinkOn) {
              $('#logo').addClass('pequenio')
+             $('#menu').addClass('blue z-depth-5')
+             $('#logo2').removeClass('invisible')
          } else {
              if ( $('#logo').hasClass('pequenio')) {
+                  $('#menu').removeClass('blue z-depth-5')
                   $('#logo').removeClass('pequenio')
+                  $('#logo2').addClass('invisible')
              }
+
          }
      });
 
      $('a[href^="#"]').on('click',function (e) {
           e.preventDefault();
           var target = this.hash;
+          if(target == "#2013" || target == "#2014" || target == "#2015")
+          {
+            return;
+          }
           var $target = $(target);
-
-
           $('html, body').stop().animate({
               'scrollTop': $target.offset().top
 
@@ -44,7 +51,31 @@ $(window).load(function() {
               window.location.hash = target;
           });
       });
+      $( '#2013').mouseenter( function(){  $(this)[0].src = 'img/color2013.png';} ).mouseleave( function(){  $(this)[0].src = 'img/bn2013.png';});
+      $( '#2014').mouseenter( function(){  $(this)[0].src = 'img/color2014.png';} ).mouseleave( function(){  $(this)[0].src = 'img/bn2014.png';});
+      $( '#2015').mouseenter( function(){  $(this)[0].src = 'img/color2014.png';} ).mouseleave( function(){  $(this)[0].src = 'img/bn2014.png';});
+      $('#enviar-formulario').click(function(){
+        nombre = $('#nombre')[0].value;
+        correo = $('#correo')[0].value;
+        mensaje = $('#mensaje')[0].value;
 
-    })
+        if(nombre === '' || correo === '' || mensaje === '' )
+        {
+          $('#error').addClass('visible');
+          return;
+        }
+        /*$.post('php/contacto.php',
+          {
+
+          },
+          function(data, status){
+              alert("Data: " + data + "\nStatus: " + status);
+          }
+        );*/
+
+
+      });
+
+    });
 
 });
